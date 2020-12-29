0a2d96ad1c6617010ccc7e278130ec1472af85d1
nblhuda <2018296332@isiswa.uitm.edu.my>
2020-12-29 08:26:10 +0800

    concat

---
+++ concat.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <?php
+           $text1 = "Hello World!";
+           $text2="What a nice day!";
+            
+        //    to concat 2 string
+           echo $text1 . " " . $text2;
+
+        ?>
+    </body>
+</html>
