695cacd2d65af022d13034123338757fa0342bb8
nblhuda <2018296332@isiswa.uitm.edu.my>
2020-12-29 08:26:32 +0800

    diplay in php (echo)

---
+++ display.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+            echo "Hello World<br>";
+            // $ is to declare
+            $name= "Nabila";
+
+            echo $name;
+        ?>
+    </body>
+</html>
