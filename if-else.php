46a613792ccb6423f12383ee28c744bd3056034c
nblhuda <2018296332@isiswa.uitm.edu.my>
2020-12-29 08:25:54 +0800

    if else statement

---
+++ if-else.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <?php
+            $d=Date("D");
+
+            if($d=="Sat")
+                echo "Have a nice weekend";
+
+            else
+                echo "Have a nice day";
+        ?>
+    </body>
+</html>
