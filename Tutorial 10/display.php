93230b63dabe413c87e1c144bf096860ed3d8bbd
nblhuda <2018296332@isiswa.uitm.edu.my>
2021-02-14 17:13:41 +0800

    php if else statement

--- Tutorial 10/display.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <body>
-    Marks: <?php echo $_POST['mark'];
-    if($_POST['mark']>=80 && $_POST['mark']<=100)
-        echo "[Excellent]";
-
-    </body>
-</html>
--- Tutorial 10/grader.php
+++ Tutorial 10/grader.php
@@ -1,12 +1,19 @@
 <html>
   <body>  
-   
-  
-    <form action="display.php" method="post">  
+    <form method="post">  
         Please input your mark: <input type="text" name="mark"> 
         <br>
         <input type="submit" name="grading" value="Grading">  
     </form>
- 
+    <?php //0.25m
+        //Retrieve name from query string and store to a local variable
+        $mark = $_POST['mark']; //0.5m
+        if( $mark > 80 ) //0.5m
+          echo "<b>Marks: $mark - (Excellent)<b>"; //0.5m
+        else if( $mark > 40 ) //0.5m
+          echo "<b>Marks: $mark - (Pass)<b>"; //0.5m
+        else //0.5m
+          echo "<b>Marks: $mark - (Fail)<b>"; //0.5m
+        ?>
   </body>
 </html>
