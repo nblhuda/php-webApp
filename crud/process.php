b2bb13c794a64d28be91ac6b7513a2d7630f675d
nblhuda <2018296332@isiswa.uitm.edu.my>
2021-02-15 00:54:44 +0800

    message if data successfully save or delete

--- crud/process.php
+++ crud/process.php
@@ -1,17 +1,30 @@
 <?php 
+    session_start();
+
     $mysqli = new mysqli("localhost", "root", "", "crudphp") or die (mysqli_error($mysqli)); 
 
+
+    // to save the data to database
     if(isset($_POST['save'])){
         $name = $_POST['name'];
         $location = $_POST['location'];
 
         $mysqli->query("INSERT INTO data (name,location) VALUES('$name', '$location')") or die ($mysqli->error);
+
+
+        $_SESSION['message'] = "Record has been saved!";
+        $_SESSION['msg_type'] = "success";
+
     }
 
     // to delete if click the button based on id 
     if(isset($_GET['delete'])){
         $id = $_GET['delete'];
+
         $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error());
+
+        $_SESSION['message'] = "Record has been deleted!";
+        $_SESSION['msg_type'] = "danger";
     }
 
 ?>
